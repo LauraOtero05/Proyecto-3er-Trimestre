60f5fefe0a0a5285721ee10152137cb1a982b0cc
Laura <lauraotero005@gmail.com>
2026-05-17 15:51:38 +0200

    Create pedido and detalle pedido entity structure

--- Proyecto_CRM/src/main/java/com/m74/proyecto_crm/repositories/PedidoRepository.java
+++ Proyecto_CRM/src/main/java/com/m74/proyecto_crm/repositories/PedidoRepository.java
@@ -1,4 +1,19 @@
 package com.m74.proyecto_crm.repositories;
 
+import com.m74.proyecto_crm.entities.Pedido;
+
+import java.util.List;
+
 public interface PedidoRepository {
+
+    Pedido findById(int id);
+
+    List<Pedido> findAll();
+
+    int save(Pedido pedido);
+
+    void update(Pedido pedido);
+
+    void deleteById(int id);
+
 }
