f4f044ba058eacc30f1db19f25d86a4dc13dbb4f
Alvarobaz <abazrodriguez@gmail.com>
2026-05-20 12:52:40 +0200

    Add ClienteRepository interface

--- Proyecto_CRM/src/main/java/com/m74/proyecto_crm/repositories/ClienteRepository.java
+++ Proyecto_CRM/src/main/java/com/m74/proyecto_crm/repositories/ClienteRepository.java
@@ -1,4 +1,18 @@
 package com.m74.proyecto_crm.repositories;
 
+import com.m74.proyecto_crm.entities.Cliente;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface ClienteRepository {
+    void save(Cliente cliente) throws SQLException;
+
+    Cliente findById(int id) throws SQLException;
+
+    List<Cliente> findAll() throws SQLException;
+
+    void update(Cliente cliente) throws SQLException;
+
+    void delete(int id) throws SQLException;
 }
