12ab850ae5cb2556f85829fd58baa37c52058f2d
Alvarobaz <abazrodriguez@gmail.com>
2026-05-20 18:59:49 +0200

    Add TrabajadorRepository interface

--- Proyecto_CRM/src/main/java/com/m74/proyecto_crm/repositories/TrabajadorRepository.java
+++ Proyecto_CRM/src/main/java/com/m74/proyecto_crm/repositories/TrabajadorRepository.java
@@ -1,4 +1,18 @@
 package com.m74.proyecto_crm.repositories;
 
+import com.m74.proyecto_crm.entities.Trabajador;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface TrabajadorRepository {
+    void save(Trabajador trabajador) throws SQLException;
+
+    Trabajador findByDni(String dni) throws SQLException;
+
+    List<Trabajador> findAll() throws SQLException;
+
+    void update(Trabajador trabajador) throws SQLException;
+
+    void delete(String dni) throws SQLException;
 }
